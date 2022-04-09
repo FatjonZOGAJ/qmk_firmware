87595b04868d88494eb071114ddaa145aa5ac1d7
FatjonZogaj <fatjon.zogaj@hotmail.com>
2022-04-09 14:44:49 +0200

    Push current version

---
+++ keyboards/splitkb/kyria/keymaps/FatjonZogaj/rules_old.mk
@@ -0,0 +1,22 @@
+OLED_DRIVER_ENABLE = no   # Enables the use of OLED displays                                                                                              
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+RGBLIGHT_ANIMATIONS = no
+WPM_ENABLE = no
+MOUSEKEY_ENABLE = no
+
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+COMMAND_ENABLE = no
+
+TAP_DANCE_ENABLE = no
+STENO_ENABLE = no
+BOOTMAGIC_ENABLE =no
+TERMINAL_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+SPACE_CADET_ENABLE = no
+KEY_LOCK_ENABLE = no
+AUDIO_ENABLE = no
+CONSOLE_ENABLE = no
+VELOCIKEY_ENABLE = no
+
